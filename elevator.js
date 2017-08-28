62995b11fa190f4121364191ed7d0f3369c08d5a
Jason Collins <the.oem@gmail.com>
2017-08-27 23:48:53 -0600

    Tests for Person construction.

--- elevator.js
+++ elevator.js
@@ -3,12 +3,22 @@ export default class Elevator {
     this.currentFloor = 0;
     this.currentRiders = [];
     this.currentRequests = [];
+    this.travelDirection = 'up';
+    this.floorsTraversed = 0;
   }
 
   reset() {
     this.currentFloor = 0;
     this.currentRiders = [];
     this.currentRequests = [];
+    this.travelDirection = 'up';
+    this.floorsTraversed = 0;
+  }
+
+  pickUpRider(rider) {
+    this.currentRiders.push(rider);
+    this.currentFloor = rider.currentFloor;
+    this.currentRequests.push(rider.dropOffFloor);
   }
 
   goToFloor(rider) {
--- tests/elevator_test.js
+++ tests/elevator_test.js
@@ -11,6 +11,15 @@ const assert = require('chai').assert;
 const Elevator = require('../elevator').default;
 const Person = require('../person').default;
 
+describe('Person', () => {
+  it('should have initial properties when created', () => {
+    const mockPerson = new Person({ name: 'Robbie', currentFloor: 2, dropOffFloor: 4 });
+    assert.equal(mockPerson.name, 'Robbie');
+    assert.equal(mockPerson.currentFloor, 2);
+    assert.equal(mockPerson.dropOffFloor, 4);
+  });
+});
+
 describe('Elevator', () => {
   const elevator = new Elevator();
 
@@ -18,17 +27,21 @@ describe('Elevator', () => {
     elevator.reset();
   });
 
-  it('should bring a rider to a floor above their current floor', () => {
-    const mockUser = new Person({ name: 'Brittany', currentFloor: 2, dropOffFloor: 5 });
-    elevator.goToFloor(mockUser);
+  describe('Level 1 - Level 2', () => {
+    it('should bring a rider to a floor above their current floor', () => {
+      const mockUser = new Person({ name: 'Brittany', currentFloor: 2, dropOffFloor: 5 });
+      // elevator.pickUpRider(mockUser);
 
-    assert.equal(elevator.currentFloor, 5);
-  });
+      elevator.goToFloor(mockUser);
 
-  it('should bring a rider to a floor below their current floor', () => {
-    const mockUser = new Person({ name: 'Brittany', currentFloor: 8, dropOffFloor: 3 });
-    elevator.goToFloor(mockUser);
+      assert.equal(elevator.currentFloor, 5);
+    });
 
-    assert.equal(elevator.currentFloor, 3);
+    it('should bring a rider to a floor below their current floor', () => {
+      const mockUser = new Person({ name: 'Brittany', currentFloor: 8, dropOffFloor: 3 });
+      elevator.goToFloor(mockUser);
+
+      assert.equal(elevator.currentFloor, 3);
+    });
   });
 });
